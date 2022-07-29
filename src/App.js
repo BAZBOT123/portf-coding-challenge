199472942ea1e390fe857e086130512e5dee876e
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-07-29 14:33:08 +0100

    bar chart not rendering

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import './App.css';
-import { ResponsiveBar } from '@nivo/bar'
+import { Bar } from '@nivo/bar'
+import data from './data.js'
 
 function App() {
-  const [data, setData] = useState(null)
-const url = 'https://api.punkapi.com/v2/beers'
+  // const [dat, setData] = useState(null)
+  // const url = 'https://api.punkapi.com/v2/beers'
 
-useEffect(() => {
-  fetch(url)
-    .then(res => res.json())
-    .then(json => {
-      console.log(json)    
-})
-},[])
-
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       console.log(json)    
+  // })
+  // },[])
 
   return (
 
-    <main>
-      <nav>
-        <button>Date Filter</button>
-        <button>Data by ABV</button>
-
-      </nav>
-
-
-    </main>
-   
+    <div className="App">
+  
+      <Bar
+        width={500}
+        height={400}
+        data={data}
+        keys={["degress"]}
+        indexBy="day"
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        padding={0.4}
+        valueScale={{ type: "linear" }}
+        colors="#3182CE"
+        animate={true}
+        enableLabel={false}
+        axisTop={null}
+        axisRight={null}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "degrees",
+          legendPosition: "middle",
+          legendOffset: -40
+        }}
+      />
+      </div>
   );
 }
 
